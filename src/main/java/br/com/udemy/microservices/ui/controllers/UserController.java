538e96aca24d0f0d74057c06a3eda2ed7971f232
viniciodf <Drhank0168>
2020-11-01 20:29:25 -0300

    microservices

--- src/main/java/br/com/udemy/microservices/ui/controllers/UserController.java
+++ src/main/java/br/com/udemy/microservices/ui/controllers/UserController.java
@@ -7,9 +7,16 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    Map<String, UserRest> users;
 
     @GetMapping
     public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
@@ -22,21 +29,27 @@ public class UserController {
             MediaType.APPLICATION_JSON_VALUE
             })
     public ResponseEntity<UserRest> getUser(@PathVariable String userId){
-        UserRest userRest = new UserRest();
-        userRest.setEmail("teste@gmail.com");
-        userRest.setFirstName("Vinicio");
-        userRest.setLastName("Ferreira");
-        return new ResponseEntity<UserRest>(userRest, HttpStatus.OK);
+
+        if(users.containsKey(userId)){
+            return new ResponseEntity<UserRest>(users.get(userId), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<UserRest>(HttpStatus.NO_CONTENT);
+        }
 
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity createUser(@RequestBody UserDetailsRequestModel userDetailsRequestModel){
-        UserRest userRest = new UserRest();
-        userRest.setEmail(userDetailsRequestModel.getEmail());
-        userRest.setFirstName(userDetailsRequestModel.getFirstName());
-        userRest.setLastName(userDetailsRequestModel.getLastName());
-        return new ResponseEntity<UserRest>(userRest, HttpStatus.OK);
+    public ResponseEntity createUser(@Valid @RequestBody UserDetailsRequestModel userDetailsRequestModel){
+        UserRest returnValue = new UserRest();
+        returnValue.setEmail(userDetailsRequestModel.getEmail());
+        returnValue.setFirstName(userDetailsRequestModel.getFirstName());
+        returnValue.setLastName(userDetailsRequestModel.getLastName());
+        if(users == null) users = new HashMap<>();
+        String userId = UUID.randomUUID().toString();
+        returnValue.setUserId(userId);
+        users.put(userId, returnValue);
+
+        return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
     }
 
     @PutMapping
--- src/main/java/br/com/udemy/microservices/ui/model/request/UserDetailsRequestModel.java
+++ src/main/java/br/com/udemy/microservices/ui/model/request/UserDetailsRequestModel.java
@@ -1,10 +1,21 @@
 package br.com.udemy.microservices.ui.model.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class UserDetailsRequestModel {
 
+    @NotNull(message = "First name cannot be null")
+    @Size(min = 2, message = "First name must be equal or grater than 2 characters")
     private String firstName;
+    @Size(min = 2, message = "Last name must be equal or grater than 2 characters")
     private String lastName;
+    @NotNull(message = "Email name cannot be null")
+    @Email
     private String email;
+    @NotNull(message = "Password name cannot be null")
+    @Size(min = 8, max = 16, message = "Password must be equal or grater than 8 characters and less than 16 characters")
     private String password;
 
     public String getFirstName() {
